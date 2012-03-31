63f243d86e08d0cca7350be05ac0a23429b6d518
Jan Kučera <kucera.ja@gmail.com>
2012-03-31 10:25:11 +0200

    Doc: okomentovano TableInput.java

--- src/jamde/table/TableInput.java
+++ src/jamde/table/TableInput.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package jamde.table;
 
 import jamde.estimator.EstimatorBuilder;
@@ -10,8 +6,9 @@ import java.util.ArrayList;
 import java.util.ArrayList;
 
 /**
- *
- * @author honza
+ * Class which is used for storing input parameters. It is almost only for use as an inner class for Table.
+ * 
+ * @author kucerj28
  */
 public class TableInput implements Cloneable{
 
@@ -25,78 +22,158 @@ public class TableInput implements Cloneable{
     private String paramsCounted; // {"first", "second", "both"}
     private ArrayList<double[]> orderErrors = new ArrayList<double[]>(); // double[]  = double[2], first number is the magnitude of the error, second is its probability
     
+    /**
+     * 
+     * @return contaminated distribution
+     */
     public Distribution getContaminated() {
         return contaminated;
     }
 
+    /**
+     * 
+     * @return contaminating distribution
+     */
     public Distribution getContaminating() {
         return contaminating;
     }
 
+    /**
+     * 
+     * @return data
+     */
     public double[] getData() {
         return data;
     }
     
+    /**
+     * 
+     * @return contamination
+     */
     public double getContamination() {
         return contamination;
     }
 
+    /**
+     * 
+     * @return estimatorBuilders
+     */
     public ArrayList<EstimatorBuilder> getEstimators() {
         return estimators;
     }
 
+    /**
+     * 
+     * @return "both" or "first" or "second"
+     */
     public String getParamsCounted() {
         return paramsCounted;
     }
 
+    /**
+     * In the tables usually K = 1000
+     * 
+     * @return sizeOfEstimator
+     */
     public int getSizeOfEstimator() {
         return sizeOfEstimator;
     }
 
+    /**
+     * In the tables usually n = {20, 50, 100, 200, 500}
+     * 
+     * @return sizeOfSamples
+     */
     public ArrayList<Integer> getSizeOfSample() {
         return sizeOfSample;
     }
 
+    /**
+     * 
+     * @return orderErrors
+     */
     public ArrayList<double[]> getOrderErrors() {
         return orderErrors;
     }
 
+    /**
+     * 
+     * @param orderErrors
+     */
     public void setOrderErrors(ArrayList<double[]> orderErrors) {
         this.orderErrors = orderErrors;
     }
 
+    /**
+     * 
+     * @param contaminated distribution
+     */
     public void setContaminated(Distribution contaminated) {
         this.contaminated = contaminated;
     }
 
+    /**
+     * 
+     * @param contaminating distribution
+     */
     public void setContaminating(Distribution contaminating) {
         this.contaminating = contaminating;
     }
 
+    /**
+     * 
+     * @param contamination
+     */
     public void setContamination(double contamination) {
         this.contamination = contamination;
     }
 
+    /**
+     * 
+     * @param data[]
+     */
     public void setData(double[] data) {
         this.data = data;
     }
     
+    /**
+     * 
+     * @param estimatorBuilders
+     */
     public void setEstimators(ArrayList<EstimatorBuilder> estimators) {
         this.estimators = estimators;
     }
 
+    /**
+     * 
+     * @param paramsCounted in {"both", "first", "second"}
+     */
     public void setParamsCounted(String paramsCounted) {
         this.paramsCounted = paramsCounted;
     }
 
+    /**
+     * In the tables usually K = 1000
+     * 
+     * @param sizeOfEstimator
+     */
     public void setSizeOfEstimator(int sizeOfEstimator) {
         this.sizeOfEstimator = sizeOfEstimator;
     }
 
+    /**
+     * In the tables usually n = {20, 50, 100, 200, 500}
+     * 
+     * @param sizeOfSamples
+     */
     public void setSizeOfSample(ArrayList<Integer> sizeOfSample) {
         this.sizeOfSample = sizeOfSample;
     }
     
+    /**
+     * 
+     * @return clone of this tableInput
+     */
     @Override
     protected TableInput clone() {
         TableInput input = new TableInput();
