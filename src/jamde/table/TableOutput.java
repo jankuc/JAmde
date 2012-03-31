4c181e4ae818c34d246b000fea936ee7e3683f82
Jan Kučera <kucera.ja@gmail.com>
2012-03-31 10:24:54 +0200

    Doc: okomentovano TableOutput.java

--- src/jamde/table/TableOutput.java
+++ src/jamde/table/TableOutput.java
@@ -8,14 +8,21 @@ import jamde.estimator.EstimatorBuilder;
 import java.util.ArrayList;
 
 /**
- *
- * @author honza
+ * Class is used as an inner class in Table. It is used to hold all the counted 
+ * statistics. It is then used as a source of data by printing procedures.
+ * Field parameter is ParameterStatistics[estimators][sizeOfSample][numOfPars]
+ * 
+ * @author kucerj28
  */
 public class TableOutput {
     private ArrayList<EstimatorBuilder> estimators;
     private ArrayList<Integer> sizeOfSample;
     private ParameterStatistics[][][] parameter;
 
+    /**
+     * Inner class which has all the necessary statistics counted in it.
+     * At this time supported are: meanValue, deviation, efficiency, error
+     */
     private class ParameterStatistics {
         public double meanValue = 1;
         public double deviation = 1;
@@ -32,6 +39,13 @@ public class TableOutput {
 
     // for outer world there are parameters 1 and 2, here it is 0 and 1. 
     
+    /**
+     * 
+     * 
+     * @param estimators
+     * @param sizeOfSample
+     * @param numOfPars
+     */
     public TableOutput(ArrayList<EstimatorBuilder> estimators, ArrayList<Integer> sizeOfSample, int numOfPars) {
         this.estimators = estimators;
         this.sizeOfSample = sizeOfSample;
@@ -40,44 +54,100 @@ public class TableOutput {
             for (int j = 0; j < sizeOfSample.size(); j++){
                 for (int k = 0; k < numOfPars; k++) {
                     parameter[i][j][k] = new ParameterStatistics();
-//                    parameter[i][j][k].deviation = 0;
-//                    p3.efficiency = 0;
-//                    p3.error = 0;
-//                    p3.meanValue = 0;
                 }
             }
         }
         
     }
     
+    /**
+     * Sets value to the meanValue field of the parameterStatistics[e][sizeOfSample][par] 
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @param value
+     */
     public void setMeanValue(EstimatorBuilder e, int sizeOfSample, int par, double value) {
         parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].meanValue = value;
     }
 
+    /**
+     * Sets value to the deviation field of the parameterStatistics[e][sizeOfSample][par] 
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @param value
+     */
     public void setDeviation(EstimatorBuilder e, int sizeOfSample, int par, double value) {
         parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].deviation = value;
     }
     
+    /**
+     * Sets value to the efficiency field of the parameterStatistics[e][sizeOfSample][par] 
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @param value
+     */
     public void setEfficiency(EstimatorBuilder e, int sizeOfSample, int par, double value) {
         parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].efficiency = value;
     }
 
+    /**
+     * Sets value to the error field of the parameterStatistics[e][sizeOfSample][par] 
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @param value
+     */
     public void setError(EstimatorBuilder e, int sizeOfSample, int par, double value) {
         parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].error = value;
     }
 
+    /**
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @return parameterStatistics[e][sizeOfSample][par].meanValue
+     */
     public double getMeanValue(EstimatorBuilder e, int sizeOfSample, int par) {
         return parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].meanValue;
     }
     
+    /**
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @return parameterStatistics[e][sizeOfSample][par].deviation
+     */
     public double getDeviation(EstimatorBuilder e, int sizeOfSample, int par) {
         return parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].deviation;
     }
     
+    /**
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @return parameterStatistics[e][sizeOfSample][par].efficiency
+     */
     public double getEfficiency(EstimatorBuilder e, int sizeOfSample, int par) {
         return parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].efficiency;
     }
 
+    /**
+     * 
+     * @param e
+     * @param sizeOfSample
+     * @param par
+     * @return parameterStatistics[e][sizeOfSample][par].error
+     */
     public double getError(EstimatorBuilder e, int sizeOfSample, int par) {
         return parameter[estimators.indexOf(e)][this.sizeOfSample.indexOf(sizeOfSample)][par-1].error;
     }
