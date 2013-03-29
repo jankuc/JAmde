7a03f8f3a1665569913548f57b32206c62d047f0
Jan Kučera <kucera.ja@gmail.com>
2013-03-29 23:40:48 +0100

    smazan stary matlabi skript

--- matlabiSkript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-	echo $1
-	cmd="C = load('$1'); f = figure('visible','off'); imagesc(C); colorbar; saveas(f,'$1.png','png'); quit"
-	#echo $cmd
-	matlab -nodisplay -r "$cmd"
-    
-                    
