45e2cda1c489a84d5e32620964d7b643b2fb14fe
Jan Kučera <kucera.ja@gmail.com>
2013-04-11 11:10:09 +0200

    pretypovani integeru na double. Vypada, ze funguje.

--- src/main/java/jamde/estimator/KolmogorovEstimator.java
+++ src/main/java/jamde/estimator/KolmogorovEstimator.java
@@ -6,6 +6,7 @@ package jamde.estimator;
 
 import jamde.MathUtil;
 import jamde.distribution.Distribution;
+import java.util.Arrays;
 
 /**
  *
@@ -19,12 +20,13 @@ public class KolmogorovEstimator extends Estimator{
     
     @Override
     public double countDistance(Distribution distr, double[] data) {
-        double dist[] = {0,0,0};
+        double dist[] = {-1,-1,-1};
         double y;
+        Arrays.sort(data);
         for (int i = 0; i < data.length; i++) {
             y = distr.getFunctionValue(data[i]);
-            dist[1] = Math.abs(y - (i/data.length));
-            dist[2] = Math.abs(y - ((i+1)/data.length));
+            dist[1] = Math.abs(y - (((double) i)/data.length));
+            dist[2] = Math.abs(y - (((double) (i+1))/data.length));
             dist[0] = MathUtil.max(dist);
         }
         return dist[0];
