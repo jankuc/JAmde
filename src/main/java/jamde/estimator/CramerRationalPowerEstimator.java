991e4cd5d37d24af7b495818be9b8c250fd86b40
Jan Kučera <kucera.ja@gmail.com>
2013-04-11 11:05:42 +0200

    CramRationalPower: Vsechny mozne integery ve vypoctu vzdalenosti pretypovany za behu na double a tvari se to, ze to dava spravne vysledky. Timto bych chtel podekovat Luccce za vyborny tip na přetyp.

--- src/main/java/jamde/estimator/CramerRationalPowerEstimator.java
+++ src/main/java/jamde/estimator/CramerRationalPowerEstimator.java
@@ -7,6 +7,7 @@ package jamde.estimator;
 import jamde.distribution.Distribution;
 import java.util.ArrayList;
 import jamde.OtherUtils;
+import java.util.Arrays;
 
 /**
  * Generalized Cramer. Instead of (F-Fn)^2 it is (F-Fn)^(p/q)
@@ -25,28 +26,27 @@ public class CramerRationalPowerEstimator extends Estimator{
     public double countDistance(Distribution distr, double[] data) {
         int p = (int) Math.round(this.par.get(0));
         int q = (int) Math.round(this.par.get(1));
-        double dist = 0;
-        double y;
-        double a;
-        double b;
+        double a,b,y, dist = 0;
+
+        Arrays.sort(data);
 
         for (int i = 0; i < data.length; i++) {
             y = distr.getFunctionValue(data[i]);
 
-            if ((y - i) / data.length < 0) {
+            if (y - ((i) * 1.0) / data.length < 0) {
                 a = -1.0;
             } else {
                 a = 1.0;
             }
-            if (y - (i + 1) / data.length < 0) {
+            if (y - ((i + 1) * 1.0) / data.length < 0) {
                 b = -1.0;
             } else {
                 b = 1.0;
             }
-            dist = dist + a * Math.pow(Math.abs(y - (i) / data.length), (double) p / q + 1) - b * Math.pow(Math.abs(y - (i + 1) / data.length), (double) p / q + 1);
+            dist = dist + a * Math.pow(Math.abs(y - ((double) i) / data.length), ((double) p) / q + 1.0) - b * Math.pow(Math.abs(y - (i + 1.0) / data.length), ((double) p) / q + 1.0);
         }
-        dist = dist / (p / q + 1);
-        
+        dist =  dist / (((double) p) / q + 1.0);
+
         return dist;
     }
 
