d3a69312f4c9285f1ebd1feda84d77077c0bc40a
Jan Kučera <kucera.ja@gmail.com>
2013-04-11 14:36:45 +0200

    pretypovani integeru na double. Vypada, ze funguje.

--- src/main/java/jamde/estimator/KolmCramFRandEstimator.java
+++ src/main/java/jamde/estimator/KolmCramFRandEstimator.java
@@ -17,6 +17,21 @@ public class KolmCramFRandEstimator extends Estimator{
     
     int[] selection;
     
+    /**
+     * 
+     * m = 2*f*n
+     * 
+     * @param p
+     * @param q
+     * @param f... in (0,1) is part of 2n.
+     * @param k
+     * @param exp
+     * @param selection 
+     */
+    public KolmCramFRandEstimator(int p, int q, double f, double k, double exp) {
+        this(p, q, f, k, exp, new int[] {1,2,3,4,5,6,7,8,9,10});
+    }
+    
     public KolmCramFRandEstimator(int p, int q, double f, double k, double exp, int[] selection) {
         this.par = new ArrayList<>();
         this.par.add((double) p); 
@@ -29,10 +44,10 @@ public class KolmCramFRandEstimator extends Estimator{
     
     @Override
     public double countDistance(Distribution distr, double[] data) {
-        int p = (int) Math.round(getPar(0));
-        int q = (int) Math.round(getPar(1));
+        double p =  Math.round(getPar(0));
+        double q =  Math.round(getPar(1));
         double m = 2* data.length * getPar(2); // m = 2*n*f
-        int k = (int) Math.round(getPar(3));
+        double k = getPar(3);
         double exp = getPar(4);
         double dist = 0;
         double y;
@@ -40,10 +55,12 @@ public class KolmCramFRandEstimator extends Estimator{
         double[] d = new double[2*data.length];
         double h = Math.min(m-2,2 * data.length - 1);
 
+        Arrays.sort(data);
+        
         for (int i = 0; i < data.length; i++) {
             y = distr.getFunctionValue(data[i]);
-            d[i] = Math.pow(Math.abs((i + 1) / data.length - y), (double) p / q);
-            d[2 * data.length - 1 - i] = Math.pow(Math.abs(i / data.length - y), (double) p / q);
+            d[i] = Math.pow(Math.abs((((double)i) + 1) / data.length - y), ((double) p) / q);
+            d[2 * data.length - 1 - i] = Math.pow(Math.abs(((double)i) / data.length - y), ((double) p) / q);
         }
 
         Arrays.sort(d); // nebo naopak?
@@ -51,10 +68,10 @@ public class KolmCramFRandEstimator extends Estimator{
         int U;
         for (int i = 0; i <= h; i++) {
             U = selection[i];
-            dist = dist + Math.pow(2 * data.length - U, exp) * d[U];
+            dist = dist + Math.pow(2.0 * data.length - U, exp) * d[U];
         }
         
-        dist = dist / Math.pow(m, exp + 1);
+        dist = dist / Math.pow(m, exp + 1.0);
         dist = dist + d[2* data.length - 1] / (k * m);
         
         return dist;
@@ -64,5 +81,7 @@ public class KolmCramFRandEstimator extends Estimator{
     public String getClassicTableName() {
         return("$ \\mathrm{KCFR}^\\frac{p}{q}_{2fn, k}, p="+this.getPar(0) + ", \\quad q="+OtherUtils.num2str(getPar(1)) + ", \\quad f=" +OtherUtils.num2str(getPar(2)) + ", \\quad k=" +OtherUtils.num2str(getPar(3)) + "$");
     }
+
+    
     
 }
